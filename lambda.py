55660fa0caae6006d5d94e74adb833c3d9776bb5
Parth Parmar <parth3rdeye@gmail.com>
2025-12-07 20:41:08 +0530

    lambda function in python

---
+++ lambda.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+
+print(add(5,6))
+
+add = lambda x,y: x+y
+
+print(add(5,6))
+
+sub = lambda x,y: x-y
+print(sub(5,6))
+
+mul = lambda x,y,z: x*y*z
+print(mul(5,6,2))

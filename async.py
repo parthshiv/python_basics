56638791bc35924eaa3a34b24a1be73c75696f5c
Parth Parmar <parth3rdeye@gmail.com>
2025-12-12 19:04:51 +0530

    use of Threads(Multi-Threads) in Python

--- async.py
+++ async.py
@@ -1,3 +1,8 @@
+"""
+Async is called as Non-Blocking I/O. A single thread runs many tasks
+Example: One person is downloading all images. While waiting for network for image 1, switches to image 2, then 3, etc. One worker handles everything efficiently. One worker, switches between tasks while waiting.
+Pros: Memory efficient. Handles thousands of task all together easily.
+"""
 import asyncio # this is in-built library to use async operations
 import time
 
@@ -16,7 +21,7 @@ def main():
 
 async def send_email(user):
     print(f"Sending to {user}...")
-    await asyncio.sleep(3) # await means: “Wait here until this task finishes.”
+    await asyncio.sleep(3) # await means: “Wait here until this task finishes.”  asyncio.sleep will not block the task, whereas time.sleep will block the task. this is Simulate delay i.e takes time to send email  
     print(f"Done {user}")
 
 async def main():
---
+++ threads.py
@@ -0,0 +1,32 @@
+"""
+Thread are called as Blocking I/O but runs concurrently.
+example: You hire 5 people, each downloads one image at the same time. Each person still “waits” for 3 seconds, but all run concurrently. Multiple workers, each waiting for their task
+Pros: good for Small number of tasks
+Cons: Memory-heavy; Extra Memory uses
+"""
+
+import threading
+import time
+
+# Simulate sending email
+def send_email(user):
+    print(f"Sending email to {user}...")
+    time.sleep(3)  # Simulate delay (like sending real email)
+    print(f"Email sent to {user}")
+
+# List of users
+users = ["Alice", "Bob", "Charlie", "David"]
+
+# Create threads for each user
+threads = []
+
+for user in users:
+    t = threading.Thread(target=send_email, args=[user])
+    threads.append(t)
+    t.start()  # Start the thread. it runs in the background
+
+# Wait for all threads to finish
+for t in threads:
+    t.join() # Wait for particular thread to finish
+
+print("All emails sent!")

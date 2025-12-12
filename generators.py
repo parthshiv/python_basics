ebce46df45b3150ae05bd78d4994b31abeca1ac3
Parth Parmar <parth3rdeye@gmail.com>
2025-12-12 13:47:12 +0530

    generators in python

---
+++ generators.py
@@ -0,0 +1,14 @@
+def squares(n):
+    for i in range(n):
+        yield i**2  # produces values one by one
+
+
+# for loop over a generator → taking one slice at a time
+for num in squares(100):
+    print(num)
+
+# for loop over a list → eating the whole pizza at once
+for i in range(100):
+        print(i**2)
+
+# generators are 'lazy'  in python. So its use is good to save memory. For loops are saving memory when iterations are more.       
